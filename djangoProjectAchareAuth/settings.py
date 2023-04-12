553f3f6a5e4e3c0481acb63d5e741d004c8be608
mfouladi <mohmmadfouladi94@gmail.com>
2023-04-12 15:16:16 +0330

    ADD: auth config

--- djangoProjectAchareAuth/settings.py
+++ djangoProjectAchareAuth/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'achare_auth.apps.AchareAuthConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
